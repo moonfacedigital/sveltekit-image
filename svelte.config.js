1226673f305a6996eeafa616bafe0883e4a70216
kylesloper <kylesloper@gmail.com>
2023-03-05 21:17:28 +0000

    :ambulance: fixed alias

--- svelte.config.js
+++ svelte.config.js
@@ -6,9 +6,11 @@ import bunAdapter from 'svelte-adapter-bun'
 import { readFileSync } from 'fs'
 import module from 'module'
 
+// Fix node errors with Sharp
 module.Module._extensions['.js'] = function (module, filename) {
   module._compile(readFileSync(filename, 'utf8'), filename)
 }
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   // Consult https://kit.svelte.dev/docs/integrations#preprocessors
@@ -25,8 +27,8 @@ const config = {
         : autoAdapter(),
     alias: {
       // this will match a file
-      $app: '/$app/',
-      $lib: '/src/lib/',
+      $app: './$app/',
+      $lib: './src/lib/',
     },
   },
 }
