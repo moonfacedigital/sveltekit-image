276e2b45cba9175e8a5713c3b161410bab72aae4
kylesloper <kylesloper@gmail.com>
2023-03-06 14:01:45 +0000

    :construction:

--- src/routes/api/_image/+server.ts
+++ src/routes/api/_image/+server.ts
@@ -1,5 +1,7 @@
-import { requestHandler } from 'sveltekit-image/api'
+import { requestHandler } from '$lib/api'
 
 import type { RequestHandler } from '@sveltejs/kit'
 
-export const GET: RequestHandler = requestHandler({})
+export const GET: RequestHandler = requestHandler({
+  productionUrl: 'https://fix-loading--leafy-marshmallow-d81e57.netlify.app',
+})
